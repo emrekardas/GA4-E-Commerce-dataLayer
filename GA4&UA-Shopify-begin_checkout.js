0a017b8c07eca09277605317e17d4108e68d21e8
Emre KARDAŞ <eekardass@yahoo.com>
2022-11-01 22:14:59 +0300

    Shopify Tracking Codes

---
+++ GA4&UA-Shopify-begin_checkout.js
@@ -0,0 +1,33 @@
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+   "event": "begin_checkout",
+   "event_id": "eb301cd2-7695-4241-90d2-2d04a6b2b6f1",
+   "ecommerce": {
+      "currencyCode": "USD",
+      "products": [
+         {
+            "id": "HP22_GLOOPASH_BLORG",
+            "name": "Gloopy Ashtray by Seth in Blue & Orange",
+            "brand": "Houseplant commerce",
+            "category": "",
+            "variant": "Default Title",
+            "price": "285.0",
+            "quantity": "1",
+            "list": "",
+            "product_id": "7107985604815",
+            "variant_id": "42169013895375",
+            "image": "//cdn.shopify.com/s/files/1/0588/0070/1647/products/DSC05058_RetouchingTest2_MinimizedShadow2048_small.jpg?v=1666977210",
+            "step" :"1"
+         }
+      ]
+   },
+   "event_time": "2022-11-01T19:12:22.278Z",
+   "marketing": {
+      "_ga": "GA1.2.2061424520.1667329616",
+      "_gid": "GA1.2.2069676990.1667329616",
+      "_fbp": "fb.1.1667329616855.1596813943",
+      "_ga_03V90HEGJR": "GS1.1.1667329616.1.1.1667329941.0.0.0",
+      "user_id": "aea92637-62be-4853-9b2d-ac6e24eb64af"
+   },
+   "gtm.uniqueEventId": 6
+});
---
+++ GA4&UA-Shopify-view_item.js
@@ -0,0 +1,32 @@
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+        "event": "view_item",
+        "event_id": "b6799eb4-ace0-4e39-bce6-e51d0045ac6e",
+        "ecommerce": {
+           "currencyCode": "USD",
+           "products": 
+            {
+               "id": "HP22_GLOOPASH_BLORG",
+               "name": "Gloopy Ashtray by Seth in Blue & Orange",
+               "brand": "Houseplant commerce",
+               "category": "",
+               "variant": "Default Title",
+               "price": "285.0",
+               "list": "",
+               "product_id": "7107985604815",
+               "variant_id": "42169013895375",
+               "compare_at_price": "0.0",
+               "image": "//cdn.shopify.com/s/files/1/0588/0070/1647/products/DSC05058_RetouchingTest2_MinimizedShadow2048.jpg?v=1666977210",
+               "inventory": "535"
+            }
+        },
+        "event_time": "2022-11-01T19:07:11.403Z",
+        "marketing": {
+           "_ga": "GA1.2.2061424520.1667329616",
+           "_gid": "GA1.2.2069676990.1667329616",
+           "_fbp": "fb.1.1667329616855.1596813943",
+           "_ga_03V90HEGJR": "GS1.1.1667329616.1.1.1667329630.0.0.0",
+           "user_id": "aea92637-62be-4853-9b2d-ac6e24eb64af"
+        },
+        "gtm.uniqueEventId": 13
+});
